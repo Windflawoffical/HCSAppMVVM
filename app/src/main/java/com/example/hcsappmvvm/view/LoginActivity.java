5a32dc2331eca56fca33e94335940daf5102bf1d
Windflaw <k.valikov@mail.ru>
2022-09-26 17:45:09 +0300

    Add login and signup activity

--- app/src/main/java/com/example/hcsappmvvm/view/LoginActivity.java
+++ app/src/main/java/com/example/hcsappmvvm/view/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.example.hcsappmvvm;
+package com.example.hcsappmvvm.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.hcsappmvvm.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
